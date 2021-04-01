5ff4d22e213d5124c676bc6d359a9dd90d19a583
Fvrom <from.ida@hotmail.com>
2021-04-01 08:46:04 +0200

    structure in viewvs

--- resources/layouts/master.blade.php
+++ resources/layouts/master.blade.php
@@ -4,9 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Your best beer app</title>
 </head>
 <body>
     <header>
-
+        @yield('header');
     </header>
+    <div class="container">
+
+        @yield('content');
+        <!-- This is a container for content
+        Yield is used to display the contents of a given section-->
+
+    </div>
+
+
+</body>
+</html>
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,34 +1,37 @@
-{{-- @extends('layouts.master')
-@section('content')
+@extends('layouts.master')
 
-@yield('content')
+
+@section('content')
+    {{-- This is the body content, within the container --}}
+    <section class="post-forms">
+        <div class="post-container">
+
+            <form action="/login" method="post">
+            @csrf
+
+                <div>
+                    <label for="email">Email</label>
+                    <input name="email" id="email" type="email" />
+                </div>
+                <div>
+                    <label for="password">Password</label>
+                    <input name="password" id="password" type="password" />
+                </div>
+
+                <button type="submit">Login</button>
+
+            </form>
+
+        <a href="/register"> Not a user? Register here </a>
+        </div>
+    </section>
 @error('email')
 <div class="">
     {{ $message }}
 </div>
-@enderror --}}
+@enderror
 
-<section class="post-forms">
-     <div class="post-container">
 
-        <form action="/login" method="post">
-        @csrf
 
-            <div>
-                <label for="email">Email</label>
-                <input name="email" id="email" type="email" />
-            </div>
-            <div>
-                <label for="password">Password</label>
-                <input name="password" id="password" type="password" />
-            </div>
-
-            <button type="submit">Login</button>
-
-        </form>
-
-       <a href="/register"> Not a user? Register here </a>
-    </div>
-</section>
-
-{{-- @endsection --}}
+ @endsection
+ {{-- This ends the body --}}
