e3c11960ee3f95b6c22dec8782800f59b2b63bfd
moasannacatharina <70512179+moasannacatharina@users.noreply.github.com>
2021-04-02 10:17:30 +0200

    Update dashboard.blade.php, added DrinkController

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,16 @@
+@extends('layouts.master')
+
+@section('content')
+
 <p>Hello, {{ $user->name }}! Do you want to <a href="logout">logout</a>?</p>
 
+
+<form action="search" method="get">
+    <div>
+        <label for="search">Search for drinks</label>
+        <input name="search" id="search" type="text" />
+    </div>
+</form>
 @include('errors')
+
+@endsection
