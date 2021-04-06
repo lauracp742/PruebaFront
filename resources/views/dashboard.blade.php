63fd03ff6dcf45ae73860b308e00df82ebb23d44
moasannacatharina <70512179+moasannacatharina@users.noreply.github.com>
2021-04-06 12:31:15 +0200

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,49 +5,39 @@
 
 
 <div class="container">
-
-<form action="search" method="GET">
-    <div>
-        <label for="search">Search for drinks</label>
-        <input name="search" id="search" type="text" />
-        <button type="submit">Search</button>
-    </div>
-</form>
-
-{{-- @dd($response) --}}
-
-
-
-@if($response ?? '')
-    @if($response['drinks'] === null )
-        @error('search')
-        <div class="">
-            {{ $message }}
+    <form action="search" method="GET">
+        <div>
+            <label for="search">Search for drinks</label>
+            <input name="search" id="search" type="text" />
+            <button type="submit">Search</button>
         </div>
-        @enderror
-    @else
-    @foreach($response['drinks'] as $drink)
-     <div class="drink">
-        <h2> {{ $drink['strDrink'] }}  </h2>
-        <img src=" {{ $drink['strDrinkThumb'] }}" />
-        <form action="/favorites" method="POST">
-            @csrf
-            <input type="hidden" name="image" value=" {{ $drink['strDrinkThumb'] }}" />
-            <input type="hidden" name="name" value=" {{ $drink['strDrink'] }}" />
-            <button type="submit">Like</button>
-        </form>
+    </form>
 
-    </div>
-
-    @endforeach
-@endif
-@endif
+    @if($response ?? '')
+        @if($response['drinks'] === null )
+            @error('search')
+            <div class="">
+                {{ $message }}
+            </div>
+            @enderror
+        @else
+            @foreach($response['drinks'] as $drink)
+            <div class="drink">
+                <h2> {{ $drink['strDrink'] }}  </h2>
+                <img src=" {{ $drink['strDrinkThumb'] }}" />
+                <form action="/favorites" method="POST">
+                    @csrf
+                    <input type="hidden" name="image" value=" {{ $drink['strDrinkThumb'] }}" />
+                    <input type="hidden" name="name" value=" {{ $drink['strDrink'] }}" />
+                    <button type="submit">Like</button>
+                </form>
+            </div>
+            @endforeach
+        @endif
+    @endif
 
 
 @include('errors')
-
-
-
 
 
 @endsection
